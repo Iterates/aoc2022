3d6bb645d6b7b889795779261ecdecf7a008425a
unknown <ariel@devocean-solutions.com>
2023-02-06 23:54:29 -0500

    Setup and day one

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+use std::fs;
+use std::io;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    day_one()?;
+    Ok(())
+}
+
+fn day_one() -> io::Result<()> {
+    let str = fs::read_to_string("./inputs/day_one.txt")?;
+    let v_calories = str.split("\r\n");
+    let mut best_sum: u32 = 0;
+    let mut best_elf: usize = 0;
+    let mut partial_sum: u32 = 0;
+
+    for (i, calories) in v_calories.enumerate() {
+        match calories.parse::<u32>() {
+            Ok(i_calories) => {
+                partial_sum += i_calories;
+            }
+            Err(_error) => {
+                if partial_sum > best_sum {
+                    best_elf = i;
+                    best_sum = partial_sum;
+                    partial_sum = 0;
+                }
+            }
+        }
+    }
+    println!("Best elf {}, with calorie count {}", best_elf, best_sum);
+    Ok(())
+}
