3c2abce56836c1c5660c78c88df0b93082cacc32
Ariel <ariel@devocean-solutions.com>
2023-02-17 14:03:29 -0500

    Validated day one part one, part two done

--- day_one/src/main.rs
+++ day_one/src/main.rs
@@ -12,6 +12,7 @@ fn day_one() -> io::Result<()> {
     let mut best_sum: u32 = 0;
     let mut best_elf: usize = 0;
     let mut partial_sum: u32 = 0;
+    let mut most_calories: Vec<u32> = vec![];
 
     for (i, calories) in v_calories.enumerate() {
         match calories.parse::<u32>() {
@@ -22,11 +23,15 @@ fn day_one() -> io::Result<()> {
                 if partial_sum > best_sum {
                     best_elf = i;
                     best_sum = partial_sum;
-                    partial_sum = 0;
                 }
+                most_calories.push(partial_sum);
+                partial_sum = 0;
             }
         }
     }
+    most_calories.sort();
+
     println!("Best elf {}, with calorie count {}", best_elf, best_sum);
+    println!("Calorie total for first three elves {:?}", &most_calories[most_calories.len() - 3..].iter().sum::<u32>());
     Ok(())
 }
