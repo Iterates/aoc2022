88db5728912a9c9e7c18f8aff8d71476c77ad38f
Ariel <ariel@devocean-solutions.com>
2023-02-17 14:28:03 -0500

    Validated day one part one, finished and validated day two part two

--- day_two/src/main.rs
+++ day_two/src/main.rs
@@ -16,7 +16,8 @@ fn process_file(filename: &str, map: &mut HashMap<String, i32>) -> std::io::Resu
 }
 
 fn total_score(map: &mut HashMap<String, i32>) -> std::io::Result<()>{
-    let scores = HashMap::from([("A X".to_string(), 2), ("A Y".to_string(), 8), ("A Z".to_string(), 3), ("B X".to_string(), 1), ("B Y".to_string(), 4), ("B Z".to_string(), 9), ("C X".to_string(), 7), ("C Y".to_string(), 2), ("C Z".to_string(), 6)]);
+    let scores = HashMap::from([("A X".to_string(), 4), ("A Y".to_string(), 8), ("A Z".to_string(), 3), ("B X".to_string(), 1), ("B Y".to_string(), 5), ("B Z".to_string(), 9), ("C X".to_string(), 7), ("C Y".to_string(), 2), ("C Z".to_string(), 6)]);
+    let scores = HashMap::from([("A X".to_string(), 3), ("A Y".to_string(), 4), ("A Z".to_string(), 8), ("B X".to_string(), 1), ("B Y".to_string(), 5), ("B Z".to_string(), 9), ("C X".to_string(), 2), ("C Y".to_string(), 6), ("C Z".to_string(), 7)]);
     let (_, sum_values) = map.iter().fold((0, 0), |(acc_keys, acc_values), (key, value)|{
         (acc_keys, acc_values + value * scores.get(key).unwrap())
     });
