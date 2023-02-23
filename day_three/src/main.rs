3a3812c71506312718a4cc01be454ad16f2334c1
Ariel <ariel@devocean-solutions.com>
2023-02-22 21:11:18 -0500

    Validated day three part two

--- day_three/src/main.rs
+++ day_three/src/main.rs
@@ -1,4 +1,5 @@
-use std::{fs::{File}, io::{BufReader, BufRead, Lines}, error::Error, iter::Enumerate};
+use std::{fs::{File}, io::{BufReader, BufRead, Lines}, error::Error, iter::Enumerate, collections::HashSet};
+use std::array;
 
 fn open() -> Lines<BufReader<File>>{
     let file = File::open("../inputs/day_three.txt").expect("Failed to open file");
@@ -26,8 +27,25 @@ fn find_common_item(){
     println!("{}", total_value);
 }
 
+fn find_common_item_v2(){
+  let lines: Vec<String> = open().filter_map(|line|line.ok()).collect();
+  let mut sum = 0;
+  for i in (0..lines.len()).step_by(3){
+    let x: HashSet<char> = lines[i].chars().collect(); 
+    let y: HashSet<char> = lines[i+1].chars().collect(); 
+    let z: HashSet<char> = lines[i+2].chars().collect(); 
+    let common: HashSet<char> = x.intersection(&y).copied().collect::<HashSet<char>>().intersection(&z).copied().collect::<HashSet<char>>();
+    common.iter().for_each(|char| {
+        let char_code = *char as u32;
+        sum +=  if char_code >= 97 {char_code - 96} else {char_code - 38};  
+    })
+  }
+  println!("{}", sum);
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
-    find_common_item();
+    //find_common_item();
+    find_common_item_v2();
 
     Ok(())
 }
